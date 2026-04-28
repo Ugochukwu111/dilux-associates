bd3df9f92e5fbd62e69ee2070907f61c3ae26939
Pascal <pas111cal3@gmail.com>
2026-04-28 19:46:25 +0100

    html && css : add listing card component

---
+++ src/Components/ListingCard.jsx
@@ -0,0 +1,49 @@
+import { ArrowUpFromLine, Trash2, Pencil, MapPin } from "lucide-react";
+
+import './ListingCard.css'
+
+export default function ListingCard() {
+  return (
+
+             <div className=" flex gap-1 listing-card box">
+               <figure className="listing-image skeleton">
+                 <img src="" alt="" />
+               </figure>
+               <div className="listing-details-container  flex  flex-col justify-between gap-2">
+                 <div>
+                   <p className=" fw700 text-red listing-name">
+                     100 by 100 Leki Estate
+                   </p>
+                   <p className="listing-description">
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                     Consectetur cumque voluptate hic eveniet quidem quibusdam,
+                     dolorem quod, eaque non ad nesciunt!
+                   </p>
+                   <div className="flex items-center justify-between">
+                     <p className="text-secondary fw700">$20,000</p>
+                     <p className="text-muted fs-small text-end">
+                       <MapPin size={12} /> Benin City | Ugbowo
+                     </p>
+                   </div>
+                 </div>
+                 <div className=" flex gap-1 listing-documents-container">
+                   <span className="fs-small bg-success text-success-light">
+                     C of O
+                   </span>
+                   <span className="fs-small bg-success text-success-light">
+                     surveyed
+                   </span>
+                 </div>
+                 <div className=" flex gap-1 btn-container">
+                   <button className="btn flex-1 bg-success-light text-success">
+                     edit <Pencil size={15} />
+                   </button>
+                   <button className="btn flex-1 bg-danger text-inverse">
+                     Delete <Trash2 size={15} />
+                   </button>
+                 </div>
+               </div>
+             </div>
+        
+  )
+}
--- src/Pages/Admin/AdminUploadListing.jsx
+++ src/Pages/Admin/AdminUploadListing.jsx
@@ -1,6 +1,24 @@
+import { ArrowUpFromLine, Trash2, Pencil, MapPin } from "lucide-react";
+import ListingCard  from '../../Components/ListingCard'
+
+const businessName = import.meta.env.VITE_BUSINESS_NAME;
+
+import "./AdminUploadListing.css";
 
 export default function AdminUploadListing() {
   return (
-    <div>AdminUploadListing</div>
-  )
+    <div className="container  flex-1">
+      <div className="flex justify-between items-center">
+        <h2>{businessName} listings</h2>
+        <button className="btn bg-secondary  text-inverse">
+          Upload listings <ArrowUpFromLine size={15} />
+        </button>
+      </div>
+      <br />
+      <div className="card-container bg-muted ">
+        <ListingCard/>
+        <ListingCard/>
+      </div>
+    </div>
+  );
 }
