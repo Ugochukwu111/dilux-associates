5f17397ac7e7c60a5bb413fcb12643c73858686b
Pascal <pas111cal3@gmail.com>
2026-05-04 19:43:48 +0100

    html & css: designed page to center at desktop view

--- src/Pages/Admin/AdminIntelligencePage.jsx
+++ src/Pages/Admin/AdminIntelligencePage.jsx
@@ -6,8 +6,8 @@ import './AdminIntelligencePage.css'
 
 export default function AdminIntelligencePage() {
   return (
-    <section>
-     <nav className="admin-intelligence-page-navigation">
+    <section className="h-full">
+     <nav className="admin-intelligence-page-navigation container">
       <ul className="flex gap-2">
         <li><NavLink to = "upload-listing" className="fw700 text-red">Upload Listing</NavLink></li>
         <li><NavLink to = "open-deals" className="fw700 text-red">Open Deals</NavLink></li>
